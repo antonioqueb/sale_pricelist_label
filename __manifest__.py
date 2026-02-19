09809ac5d24c5a72a50383f76b1c028b52b3766e
Antonio Queb <cardstillo@gmail.com>
2026-02-18 21:27:23 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Sale Pricelist Label - Moneda',
+    'version': '19.0.1.0.0',
+    'summary': 'Cambia la etiqueta del campo Lista de precios a Moneda en la orden de venta',
+    'category': 'Sales',
+    'author': 'Alphaqueb Consulting SAS',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
